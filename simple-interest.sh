9d2b5c882e69232972d9f02f5b4e5fa0d0d5c05d
Prince Oduro <75932370+poduro-code@users.noreply.github.com>
2024-04-10 23:47:22 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,38 +1,10 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-   #!/bin/bash
+#!/bin/bash
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
    # Do not use this in production. Sample purpose only.
-   # Author: Princeoduro
+   # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # poduro- code
+   # <your GitHub username>
    # Input:
    # p, principal amount
    # t, time period in years
